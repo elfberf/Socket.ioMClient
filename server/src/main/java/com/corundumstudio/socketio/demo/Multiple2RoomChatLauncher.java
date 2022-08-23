26c7de83bd748c3549e08bd1ab43451ab751b222
Elif Berfin Gülbaş <110381987+elfberf@users.noreply.github.com>
2022-08-23 14:07:08 +0300

    Update Multiple2RoomChatLauncher.java

--- server/src/main/java/com/corundumstudio/socketio/demo/Multiple2RoomChatLauncher.java
+++ server/src/main/java/com/corundumstudio/socketio/demo/Multiple2RoomChatLauncher.java
@@ -11,6 +11,7 @@ public class Multiple2RoomChatLauncher {
 
     public static final String ROOM_DİGERLERİ = "digerleri";
 
+    ////defining the map strings
     private HashMap<String, HashSet<SocketIOClient>> mapRooms = new HashMap<String, HashSet<SocketIOClient>>();
     private HashMap<SocketIOClient, String> mapClientRoom = new HashMap<SocketIOClient, String>();
 
@@ -21,12 +22,13 @@ public class Multiple2RoomChatLauncher {
 
     private void start() throws InterruptedException {
         Configuration config = new Configuration();
-        config.setHostname("localhost");//192.168.254.105
+        config.setHostname("localhost");
         config.setPort(9092);
 
+         //adding socket
         final SocketIOServer server = new SocketIOServer(config);
 
-
+         //adding broadcast to server
         BroadcastOperations digerleri = server.getRoomOperations(ROOM_DİGERLERİ);
         System.out.println();
 
@@ -50,13 +52,15 @@ public class Multiple2RoomChatLauncher {
             }
         });
 
+        //create function
         server.addEventListener("create", Object.class, new DataListener<Object>() {
             @Override
             public void onData(SocketIOClient socketIOClient, Object o, AckRequest ackRequest) throws Exception {
                 System.out.println();
             }
         });
-
+        
+        //creating room
         server.addEventListener("create-room", Object.class, new DataListener<Object>() {
             @Override
             public void onData(SocketIOClient socketIOClient, Object o, AckRequest ackRequest) throws Exception {
@@ -64,18 +68,23 @@ public class Multiple2RoomChatLauncher {
             }
         });
 
+        //room change function
+        //add user list to here
         server.addEventListener("roomChange", RoomChange.class, new DataListener<RoomChange>() {
             @Override
             public void onData(SocketIOClient client, RoomChange roomChange, AckRequest ackRequest) throws Exception {
+                // creating active room which holds current room
                 String activeRoom = mapClientRoom.get(client);
+                //printing the active room and changed room to terminal
                 System.out.println(roomChange.getUserName() + ": activeRoom = " + activeRoom);
                 System.out.println(roomChange.getUserName() + ": new room = " + roomChange.getRoom());
-                // User'ı şu anda bulunduğu odadan çıkartacağız
+                // removing the client from the room which currently in
                 mapClientRoom.remove(client);
-                // User'ı room change data'sındaki odaya dahil edeceğiz
+                // adding client to data in roomChange data
                 mapClientRoom.put(client, roomChange.getRoom());
 
-                // Önceden olduğu odadan kişiyi/client'ı çıkartma
+               // removing client from the previous room
+                // by this we are making the room data private for that room
                 HashSet<SocketIOClient> socketIOClients = null;
                 if (activeRoom != null) {
                     socketIOClients = mapRooms.get(activeRoom);
@@ -84,38 +93,28 @@ public class Multiple2RoomChatLauncher {
                     }
                 }
 
-                // Yeni odaya ilgili kişiyi/client'ı ekle
+                // adding client to new room
                 String roomNew = roomChange.getRoom();
                 socketIOClients = mapRooms.get(roomNew);
-                // Liste hiç yoksa yeni liste yarat
+               // if there is no list create a new list
                 if (socketIOClients == null) {
                     socketIOClients = new HashSet<SocketIOClient>();
                     mapRooms.put(roomNew, socketIOClients);
                 }
                 socketIOClients.add(client);
-
-/*
-                Oda değişikliğini diğer üyelere haber verme
-                RoomChange change = new RoomChange(roomChange.getRoom());
-                for (SocketIOClient c : socketIOClients) {
-                    if (!client.equals(c))
-                        c.sendEvent("roomChange", change);
-                }
-*/
             }
 
         });
 
+        //chat event
         server.addEventListener("chatevent", ChatRoomObject.class, new DataListener<ChatRoomObject>() {
             @Override
             public void onData(SocketIOClient client, ChatRoomObject data, AckRequest ackRequest) {
-                // broadcast messages to all clients - tüm istemcilere mesaj yayınla
+                // broadcast messages to all clients 
                 String room = data.getRoom();
                 System.out.println("data.getRoom() = " + room);
-                // if (room != null)
                 {
-                    // server.getBroadcastOperations().sendEvent("chatevent", data);
-
+                   
                     HashSet<SocketIOClient> socketIOClients = mapRooms.get(room);
                     if (socketIOClients != null) {
                         ChatRoomObject single = new ChatRoomObject(data.getUserName(), data.getMessage());
@@ -125,13 +124,6 @@ public class Multiple2RoomChatLauncher {
                         }
                     }
                 }
-
-              /*  Collection<SocketIOClient> allClients = server.getAllClients();
-                ChatRoomObject single = new ChatRoomObject(data.getUserName(), "[TÜM] " + data.getMessage());
-                for (SocketIOClient c : allClients) {
-                    if (!client.equals(c))
-                        c.sendEvent("chatevent", single);
-                }*/
             }
         });
 
